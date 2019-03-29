ee7672823aed645d30c2e2dc1b6cc62b93cee805
woodensRangerWZJ <13029768866@163.com>
2019-03-29 22:36:55 +0800

    线上接口测试

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import http from './http'
 
 // 1、基础路径
-const BASE_URL = 'http://127.0.0.1:3000'
+const BASE_URL = 'http://154.83.12.64:3000'
 
 //2、请求方法
 export const getHomeCasual = () => http(BASE_URL + '/api/homecasual'); // 首页轮播图数据
